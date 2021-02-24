513fbc386d325f4e03d517c5860c25e0b76f9218
Roberto Guerra <uris77@gmail.com>
2021-02-24 17:08:01 -0600

    adds store bloc

--- lib/products/bloc/products_event.dart
+++ lib/products/bloc/products_event.dart
@@ -1,31 +1,40 @@
 part of 'products_bloc.dart';
 
+/// The Bloc Events for Products
 @immutable
 abstract class ProductsEvent extends Equatable {
+  /// Constructor
   const ProductsEvent();
 
   @override
   List<Object> get props => [];
 }
 
+/// The event for loading products
 class LoadProducts extends ProductsEvent {
   @override
   List<Object> get props => [];
 }
 
+/// The event for adding a product
 class AddProduct extends ProductsEvent {
-  final Product product;
-
+  /// The Constructor
   const AddProduct(this.product);
+
+  /// The product to be added
+  final Product product;
 
   @override
   List<Object> get props => [product];
 }
 
+/// The event for updating a product
 class UpdateProduct extends ProductsEvent {
-  final Product product;
-
+  /// Constructor
   const UpdateProduct(this.product);
+
+  /// The product to update
+  final Product product;
 
   @override
   List<Object> get props => [product];
--- lib/stores/bloc/stores_bloc.dart
+++ lib/stores/bloc/stores_bloc.dart
@@ -3,18 +3,42 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:scanshop_api/api.dart';
 import 'package:scanshop_models/models.dart';
 
 part 'stores_event.dart';
 part 'stores_state.dart';
 
+/// The StoresBloc. It is responsible for persisting a store and loading stores.
 class StoresBloc extends Bloc<StoresEvent, StoresState> {
-  StoresBloc() : super(StoresLoadInProgress());
+  /// Constructor
+  StoresBloc({this.storesDao}) : super(StoresLoadInProgress());
+
+  /// The Stores DAO
+  final Dao storesDao;
+
+  /// Initial State
+  StoresState get initialState => StoresLoadInProgress()
 
   @override
   Stream<StoresState> mapEventToState(
     StoresEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+   if(event is LoadStores) {
+     yield StoresLoadInProgress();
+     yield* _reloadStores();
+   } else if (event is AddStore) {
+     yield* _addStore(event);
+   }
+  }
+
+  Stream<StoresState> _reloadStores() async* {
+    final stores = await storesDao.getAllSortedByName() as List<Store>;
+    yield StoresLoadSuccess(stores);
+  }
+
+  Stream<StoresState> _addStore(AddStore event) async* {
+    await storesDao.insert(event.store);
+    yield* _reloadStores();
   }
 }
